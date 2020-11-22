c357dcd71ed4d4c71cfb8fa77e49decef0ea5769
KoaLaYT <hytohyeah@outlook.com>
2020-11-22 15:25:35 +0800

    lowest common ancestor of a binary search tree

---
+++ 20201122_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/solution.hpp
@@ -0,0 +1,16 @@
+#ifndef SOLUTION_HPP_20201122
+#define SOLUTION_HPP_20201122
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q);
+};
+
+#endif
